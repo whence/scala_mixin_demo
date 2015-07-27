b9c955c61ce0d827410a6e79c24762d77995b1ee
Wesley Li <wesley.li@myob.com>
2015-07-27 16:42:13 +1000

    add some love to clj

--- src/main/clojure/mixin_demo/src/mixin_demo/core.clj
+++ src/main/clojure/mixin_demo/src/mixin_demo/core.clj
@@ -1,56 +1,73 @@
 (ns mixin-demo.core
   (:gen-class))
 
-(defmulti ask (fn [player _ _] (:type player)))
+(defn- type1
+  "get type of the first argument"
+  [x & args] (:type x))
 
-(defmethod ask :local
+(defmulti choose
+  "let the player choose from some items"
+  type1)
+
+(defmulti send-message
+  "send message to a connection"
+  type1)
+
+(defmulti receive-message
+  "receive message from a connection"
+  type1)
+
+(def all-logs
+  "global log store"
+  (atom []))
+
+(defn- with-logging
+  "wrap a function and log its result and name of the first argument"
+  [f]
+  (fn [& args]
+    (let [result (apply f args)
+          name (:name (first args))
+          entry [name result]]
+      (println "logging" entry)
+      (swap! all-logs conj entry)
+      result)))
+
+(defmethod choose :local
   [player question items]
   (println (:name player) ":" question)
   (doseq [i items] (println i))
   (read-line))
 
-(defmulti send-message (fn [connection _] (:type connection)))
-
-(defmethod send-message :dummy
-  [connection message]
-  (println "pretend sending" message "to ip" (:ip connection)))
-
-(defmulti receive-message :type)
-
-(defmethod receive-message :dummy
-  [connection]
-  (println "pretend receiving from ip" (:ip connection))
-  :do-it)
-
-(defmethod ask :network
+(defmethod choose :network
   [player question items]
   (let [connection (:connection player)
         message (clojure.string/join "\n" (cons question items))]
     (send-message connection message)
     (receive-message connection)))
 
-(def all-logs (atom []))
-
-(defmethod ask :ai
+(defmethod choose :ai
   [player question items]
-  (println "making decision from logs" @all-logs "and cards" (:cards player))
+  (println "making decision from logs" @all-logs "and hand of" (:hand player))
   :smart-move)
 
-(def ask
-  (let [wrap (fn [ask]
-              (fn [player question items]
-                (let [choice (ask player question items)
-                      entry [(:name player) choice]]
-                  (println "logging" entry)
-                  (swap! all-logs conj entry)
-                  choice)))]
-    (wrap ask)))
+(def choose
+  "add logging capability to choose"
+  (with-logging choose))
+
+(defmethod send-message :dummy
+  [connection message]
+  (println "pretend sending" message "to ip" (:ip connection)))
+
+(defmethod receive-message :dummy
+  [connection]
+  (println "pretend receiving from ip" (:ip connection))
+  :do-it)
 
 (defn -main [& args]
-  (let [local-player {:type :local :name "local" :cards nil}
-        network-player {:type :network :connection {:type :dummy :ip "localhost"} :name "network" :cards nil}
-        ai-player {:type :ai :name "ai" :cards [:card3 :card1]}
+  (let [local-player {:type :local :name "local" :hand nil}
+        network-player {:type :network :connection {:type :dummy :ip "localhost"} :name "network" :hand nil}
+        ai-player {:type :ai :name "ai" :hand [:card3 :card1]}
         players [local-player network-player ai-player ai-player]]
     (doseq [player players]
-      (let [answer (ask player "question?" [:item1 :item2 :item3])]
+      (let [answer (choose player "question?" [:item1 :item2 :item3])]
         (println "answer of" (:name player) "is" answer)))))
